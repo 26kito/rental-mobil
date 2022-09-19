b7852729016038aa39170bb57ffa36bf14f48392
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2022-09-19 19:23:59 +0700

    validation manual

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,9 +9,25 @@ use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\HasApiTokens;
 
 class UserController extends Controller
 {
+  use HasApiTokens;
+
+  public function checkDuplicate(Request $request)
+  {
+    $emailDuplicate = User::where('email', $request->email)->get();
+    $mpDuplicate = User::where('mobile_phone', $request->mobile_phone)->get();
+
+    $message = "";
+
+    if (count($emailDuplicate)) $message .= "Email sudah ada ";
+    if (count($mpDuplicate)) $message .= "No Hp sudah ada";
+
+    return $message;
+  }
   /**
    * Display a listing of the resource.
    *
@@ -22,16 +38,18 @@ class UserController extends Controller
     $user = User::get();
     try {
       if ($user->isNotEmpty()) {
+
         return response()->json([
           'message' => 'Success',
           'data' => $user
         ], 200);
-      } else {
-        return response()->json([
-          'message' => 'There\'s no data'
-        ], 200);
       }
+
+      return response()->json([
+        'message' => 'There\'s no data'
+      ], 200);
     } catch (Exception $e) {
+
       return response()->json([
         'message' => $e->getMessage()
       ], 400);
@@ -41,6 +59,7 @@ class UserController extends Controller
   public function profile($name)
   {
     $user = User::with('role')->where('name', $name)->get();
+
     return response()->json([
       'message' => 'Profile user',
       'data' => $user
@@ -55,34 +74,40 @@ class UserController extends Controller
    */
   public function register(Request $request)
   {
-    $validated = $request->validate([
-      'name' => 'required|min:4|max:20|alpha',
-      'email' => 'required|email|unique:users',
-      'mobile_phone' => 'required|min:11|max:15|regex:/^([0-9\s\-\+\(\)]*)$/|unique:users',
-      'address' => 'required',
-      'role_id' => 'required|exists:roles,id',
-      'password' => 'required|min:8|confirmed'
+    // $messageIfDuplicate = $this->checkDuplicate($request);
+    //
+    // if ($messageIfDuplicate) {
+    //   return response()->json([
+    //     'message' => $messageIfDuplicate
+    //   ], 403);
+    // }
+
+    $validation = Validator::make($request->all(), [
+      'email' => 'required|unique:users|email',
+      'mobile_phone' => 'required|unique:users|min:11|max:15'
     ]);
 
-    try {
-      if ($validated) {
-        $user = User::create([
-          'name' => $request->name,
-          'email' => $request->email,
-          'mobile_phone' => $request->mobile_phone,
-          'address' => $request->address,
-          'role_id' => $request->role_id,
-          'password' => Hash::make($request->password)
-        ]);
-      }
-
-      $accessToken = $user->createToken('authToken')->accessToken;
+    if ($validation->fails()) {
       return response()->json([
-        'message' => 'User Created Successfully!',
-        'user' => $user,
-        'access_token' => $accessToken
+        'message' => 'No HP dan Email sudah ada'
+      ]);
+    }
+
+    try {
+      User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'mobile_phone' => $request->mobile_phone,
+        'address' => $request->address,
+        'role_id' => $request->role_id,
+        'password' => Hash::make($request->password)
+      ]);
+
+      return response()->json([
+        'message' => 'User berhasil dibuat, kamu bisa Login sekarang',
       ], 201);
     } catch (Exception $e) {
+
       return response()->json([
         'message' => $e->getMessage()
       ], 400);
@@ -94,60 +119,27 @@ class UserController extends Controller
    */
   public function login(Request $request)
   {
-    $data = $request->validate([
-      'email' => 'required|email|exists:users,email',
-      'password' => 'required'
-    ]);
-
     try {
       $user = User::where('email', $request->email)->first();
-      if (Auth::attempt($data) && Hash::check($request->password, $user->password)) {
+
+      if (Auth::attempt($request) && Hash::check($request->password, $user->password)) {
+
         $accessToken = $user->createToken('authToken')->accessToken;
         return response()->json([
-          'message' => 'Success',
-          'data' => $user,
           'access_token' => $accessToken
         ], 200);
-      } else {
-        return response()->json([
-          'message' => 'Wrong Password!'
-        ], 422);
       }
+
+      return response()->json([
+        'message' => 'Email dan password tidak sesuai'
+      ], 422);
     } catch (Exception $e) {
+
       return response()->json([
         'message' => $e->getMessage()
       ]);
     }
   }
-
-  /**
-   * Show specified user by id.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-
-  // public function show($id)
-  // {
-  //   $user = User::find($id);
-  //   try {
-  //     if ($user) {
-  //       return response()->json([
-  //         'message' => 'Success',
-  //         'data' => $user
-  //       ], 200);
-  //     } else {
-  //       return response()->json([
-  //         'message' => 'User not found'
-  //       ], 200);
-  //     }
-  //   } catch (Exception $e) {
-  //     return response()->json([
-  //       'message' => $e->getMessage()
-  //     ], 400);
-  //   }
-  // }
-
   /**
    * Function for update specified user.
    *
